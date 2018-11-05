f87241cab60f7ce0dbace1551aecf2c3cbc65163
Elisabeth Garber <elgarber@umich.edu>
2018-11-05 12:35:45 -0500

    Added test for repeat operator

--- rpn.py
+++ rpn.py
@@ -32,7 +32,8 @@ def calculate(arg):
             else:
                 val2 = int(stack.pop())
                 val1 = int(stack.pop())
-                 
+                
+                func = op[token]
                 result = func(val1, val2)
                 stack.append(str(result))
     
